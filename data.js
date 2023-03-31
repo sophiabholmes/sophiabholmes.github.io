8dccda424f0c40bf50c4d38247871b5d96b38116
sophiabholmes <123424773+sophiabholmes@users.noreply.github.com>
2023-03-31 18:39:51 -0400

    Update data.js

--- data.js
+++ data.js
@@ -48,7 +48,7 @@ var musicVideos = [{
   }, {
     title: "3",
     image: "3.png",
-    positionLeft: 40,
+    positionLeft: 30,
     positionTop: -50,
     tilt: -10,
     width: 120,
@@ -57,9 +57,9 @@ var musicVideos = [{
  }, {
     title: "3",
     image: "5.png",
-    positionLeft: 180,
+    positionLeft: 170,
     positionTop: -70,
-    tilt: -15,
+    tilt: -7,
     width: 180,
     text: false,
     vid: '<iframe src="https://player.vimeo.com/video/767921143?h=63b95ebd34&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe><p><a href="https://vimeo.com/767921143">Frankie Cosmos - Empty Head  (Official Video)</a> from <a href="https://vimeo.com/user12726649">Sophia Bennett Holmes</a> on <a href="https://vimeo.com">Vimeo</a>.</p>'
