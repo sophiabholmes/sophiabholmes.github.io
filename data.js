6ada93432d953b4e49a1dd4cb50f4c1c6a2ce79c
sophiabholmes <123424773+sophiabholmes@users.noreply.github.com>
2023-10-17 12:56:56 -0400

    Update data.js

--- data.js
+++ data.js
@@ -59,14 +59,14 @@ var musicVideos = [{
     image: "5.png",
     positionLeft: 180,
     positionTop: -70,
-    tilt: -15,
+    tilt: -20,
     width: 180,
     text: 'crosslegged - automatic (co-directed with cade featherstone) ~ 2023',
     vid: '<iframe title="vimeo-player" src="https://player.vimeo.com/video/814695587?h=82be5f306d" width="640" height="360" frameborder="0"    allowfullscreen></iframe>'
  }, {
     title: "audio",
     image: "6.png",
-    positionLeft: 115,
+    positionLeft: 100,
     positionTop: -100,
     tilt: -15,
     width: 180,
